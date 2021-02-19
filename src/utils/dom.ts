be807827f138659b556d05df637bc3ee12436568
YesIDont <mateuszfryc@gmail.com>
2021-02-19 11:06:45 +0100

    utils cleanup

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,12 @@
+export const get = <T = HTMLElement>(query: string, element = document): T | undefined => {
+    const searched: unknown = element.querySelector(query);
+    if (searched) {
+        return searched as T;
+    }
+
+    return undefined;
+};
+
 export const addClass = <T extends HTMLElement = HTMLElement>(targetElement: T, className: string): void => {
     // eslint-disable-next-line
     targetElement.className += ` ${className} `;
